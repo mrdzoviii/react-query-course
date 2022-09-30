225c5eb93acb40413540e992561641f1426f0e2d
Jovan Danilovic <danilovic.jovan@gmail.com>
2022-09-30 09:27:20 +0200

    feat: mutation done

--- src/pages/AddIssue.jsx
+++ src/pages/AddIssue.jsx
@@ -1,3 +1,46 @@
+import { useMutation, useQueryClient } from "react-query";
+import { useNavigate } from "react-router-dom";
+
 export default function AddIssue() {
-  return <h2>Add Issue</h2>;
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const addIssue = useMutation(
+    (issueBody) => {
+      return fetch(`/api/issues`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(issueBody),
+      }).then((res) => res.json());
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(["issues"], { exact: true });
+        queryClient.setQueryData(["issues", data.number.toString()], data);
+        navigate(`/issue/${data.number.toString()}`);
+      },
+    }
+  );
+  return (
+    <div className="add-issue">
+      <h2>Add Issue</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (addIssue.isLoading) return;
+          addIssue.mutate({
+            comment: e.target.comment.value,
+            title: e.target.title.value,
+          });
+        }}
+      >
+        <label htmlFor="title">Title</label>
+        <input id="title" type="text" placeholder="Title" name="title" />
+        <label htmlFor="comment">Comment</label>
+        <textarea placeholder="Comment" id="comment" name="comment" />
+        <button type="submit" disabled={addIssue.isLoading}>
+          {addIssue.isLoading ? "Adding issue..." : "Add Issue"}
+        </button>
+      </form>
+    </div>
+  );
 }
